82e8ab0216415417a99f815870930cfdcb6c5bb4
beksultan <selflearningjava@gmail.com>
2022-02-17 17:06:23 +0600

    removed authority from download file

--- src/main/java/kg/ebooks/eBook/api/FileAPI.java
+++ src/main/java/kg/ebooks/eBook/api/FileAPI.java
@@ -22,37 +22,42 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @Slf4j
 @Tag(name = "This API for saving files {images, audios, electronic_books} to amazon S3 bucket")
-@PreAuthorize("hasAnyAuthority('ADMIN', 'VENDOR')")
 public class FileAPI {
 
     private final FileService fileService;
 
     @PostMapping("/upload/image")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'VENDOR')")
     public FileInfo uploadFile(@RequestParam MultipartFile file) {
         return fileService.uploadFile(FolderName.IMAGES, file);
     }
 
     @PostMapping("/upload/audio")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'VENDOR')")
     public FileInfo uploadAudioFile(@RequestParam MultipartFile file) {
         return fileService.uploadFile(FolderName.AUDIO_FILES, file);
     }
 
     @PostMapping("/upload/pdf")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'VENDOR')")
     public FileInfo uploadPDFFile(@RequestParam MultipartFile file) {
         return fileService.uploadFile(FolderName.PDF_FILES, file);
     }
 
     @PostMapping("/upload/audio/fragment")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'VENDOR')")
     public FileInfo uploadFragmentFile(@RequestParam MultipartFile file) {
         return fileService.uploadFile(FolderName.AUDIO_FRAGMENTS_FILES, file);
     }
 
     @GetMapping("/download/{fileId}")
+    @PreAuthorize("permitAll()")
     public byte[] downloadFile(@PathVariable Long fileId) {
         return fileService.downloadFile(fileId);
     }
 
     @DeleteMapping("/delete/{fileId}")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'VENDOR')")
     public void deleteFile(@PathVariable Long fileId) {
         fileService.deleteFile(fileId);
     }
