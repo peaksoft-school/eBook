575e8c64704c8956773028b793cb163fde03c2f3
beksultan <selflearningjava@gmail.com>
2022-03-04 11:38:41 +0600

    finished find books by type

--- src/main/java/kg/ebooks/eBook/db/repository/BookRepository.java
+++ src/main/java/kg/ebooks/eBook/db/repository/BookRepository.java
@@ -2,12 +2,14 @@ package kg.ebooks.eBook.db.repository;
 
 import kg.ebooks.eBook.aws.model.FileInfo;
 import kg.ebooks.eBook.db.domain.model.books.Book;
+import kg.ebooks.eBook.db.domain.model.enums.TypeOfBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -17,4 +19,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     Book findByBookName(String bookName);
 
+    @Query("SELECT b FROM Book b WHERE b.typeOfBook = ?1")
+    List<Book> findByBookType(TypeOfBook typeOfBook);
 }
